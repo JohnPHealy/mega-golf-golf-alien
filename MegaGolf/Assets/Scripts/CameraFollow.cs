d996666bf6668ba0792ef0f464ba16af61529d67
zoheb598 <77964783+zoheb598@users.noreply.github.com>
2021-02-18 00:33:13 +0000

    Made my lava level and added then tweaked the camera follow player script

---
+++ MegaGolf/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Target;
+
+    private float speed = 10f;
+    public Vector3 Offset;
+    private void FixedUpdate()
+    {
+        Vector3 desPos = Target.position + Offset;
+        Vector3 smoPos = Vector3.Lerp(transform.position, desPos, speed * Time.deltaTime);
+        transform.position = desPos;
+
+        transform.LookAt(Target);
+
+        if (Input.GetKey("left"))
+        {
+            Offset.x = 35;
+        }
+        if (Input.GetKey("right"))
+        {
+            Offset.x = -35;
+        }
+    }
+}
